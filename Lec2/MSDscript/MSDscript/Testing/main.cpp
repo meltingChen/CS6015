c78f10ba128eb6cf0e023639195884c8d105de2e
meltingChen <142821471+meltingChen@users.noreply.github.com>
2024-01-30 09:06:54 -0700

    HW3

--- Lec2/MSDscript/MSDscript/Testing/main.cpp
+++
@@ -1,8 +0,0 @@
-//#define CATCH_CONFIG_MAIN
-//#include "catch.h"
-#define CATCH_CONFIG_RUNNER
-#include "catch.h"
-int main(int argc, char **argv) {
-    Catch::Session().run(argc, argv);
-    return 0;
-}
