96d071163b6dac9a92f7c3d11d1fca5acdb4f627
meltingChen <142821471+meltingChen@users.noreply.github.com>
2024-03-19 23:30:24 -0600

    conditional

---
+++ MSDscript/MSDscript/val.cpp
@@ -0,0 +1,8 @@
+//
+//  val.cpp
+//  MSDScript
+//
+//  Created by 陳肜樺 on 3/17/24.
+//
+
+#include "val.hpp"
---
+++ MSDscript/MSDscript/val.hpp
@@ -0,0 +1,13 @@
+//
+//  val.hpp
+//  MSDScript
+//
+//  Created by 陳肜樺 on 3/17/24.
+//
+
+#ifndef val_hpp
+#define val_hpp
+
+#include <stdio.h>
+
+#endif /* val_hpp */
